6ca2bbcd453df8c79d3a97453e75aad6f93ca8af
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-18 15:54:02 +0530

    Load to-do task from local storage on load

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,7 @@ import * as model from "./model.js";
 import listView from "./listView.js";
 
 const init = function () {
+  model.getLocalStorageDataWrapper();
   listView.renderAllTodoTasksList(
     model.state.todotasks,
     model.state.activeFilter
