16cca9779fada2a811bb5fbe6046a0bd882ee668
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-18 13:22:05 +0530

    Added a dummy render function

--- js/controller.js
+++ js/controller.js
@@ -1,2 +1,7 @@
 import * as model from "./model.js";
 import listView from "./listView.js";
+
+const init = function () {
+  listView.renderTodoTasksList(model.state.todotasks);
+};
+init();
--- js/listView.js
+++ js/listView.js
@@ -1,3 +1,9 @@
-class ListView {}
+class ListView {
+  _todoListElement = document.querySelector(".todo-list");
+  renderTodoTasksList(taskList) {
+    console.log(this._todoListElement);
+    console.log(taskList);
+  }
+}
 
 export default new ListView();
