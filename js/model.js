59915fb08208a62f9915ec925c3538fc258a7985
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-18 13:06:31 +0530

    Implemented getters and setters for localStorage

--- js/model.js
+++ js/model.js
@@ -6,3 +6,15 @@ export const state = {
     taskCount: 0,
   },
 };
+
+function setLocalStorage(state) {
+  localStorage.setItem("todoTasks", JSON.stringify(state.todotasks));
+}
+
+function getLocalStorages() {
+  const localToddTaskData = JSON.parse(localStorage.getItem("todoTasks"));
+  // Guard caluse if local storage is empty
+  if (!localToddTaskData) return;
+  // Restore from local storage
+  state.todotasks = localToddTaskData;
+}
